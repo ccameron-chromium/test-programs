05af2d84f5a093e3ee3b1984a6b37bd2c423f120
Christopher Cameron <ccameron@chromium.org>
2023-06-29 19:51:43 +0200

    Update staircase

--- staircase_generator.cc
+++ staircase_generator.cc
@@ -15,6 +15,34 @@
 uint8_t avif_irot_angle = 0;
 uint8_t avif_imir_mode = 0;
 
+const float BT2020_10bit_limited_rgb_to_yuv[] = {
+      0.224951f,  0.580575f,  0.050779f,  0.000000f,  0.062561f,
+     -0.122296f, -0.315632f,  0.437928f,  0.000000f,  0.500489f,
+      0.437928f, -0.402706f, -0.035222f,  0.000000f,  0.500489f,
+      0.000000f,  0.000000f,  0.000000f,  1.000000f,  0.000000f,
+};
+
+float GreyToY(float x) {
+  return BT2020_10bit_limited_rgb_to_yuv[0] * x +
+         BT2020_10bit_limited_rgb_to_yuv[1] * x +
+         BT2020_10bit_limited_rgb_to_yuv[2] * x +
+         BT2020_10bit_limited_rgb_to_yuv[4];
+}
+
+float GreyToU(float x) {
+  return BT2020_10bit_limited_rgb_to_yuv[5 + 0] * x +
+         BT2020_10bit_limited_rgb_to_yuv[5 + 1] * x +
+         BT2020_10bit_limited_rgb_to_yuv[5 + 2] * x +
+         BT2020_10bit_limited_rgb_to_yuv[5 + 4];
+}
+
+float GreyToV(float x) {
+  return BT2020_10bit_limited_rgb_to_yuv[10 + 0] * x +
+         BT2020_10bit_limited_rgb_to_yuv[10 + 1] * x +
+         BT2020_10bit_limited_rgb_to_yuv[10 + 2] * x +
+         BT2020_10bit_limited_rgb_to_yuv[10 + 4];
+}
+
 void DumpPixelBuffer(const char* filename, uint16_t trc) {
   const int kNumSteps = 17;
   int width = 64 * kNumSteps;
@@ -26,10 +54,10 @@ void DumpPixelBuffer(const char* filename, uint16_t trc) {
     avifImage * image = avifImageCreate(width, height, 10, AVIF_PIXEL_FORMAT_YUV420);
     image->colorPrimaries = AVIF_COLOR_PRIMARIES_BT2020;
     image->transferCharacteristics = trc; // AVIF_TRANSFER_CHARACTERISTICS_HLG;
-    image->matrixCoefficients = AVIF_MATRIX_COEFFICIENTS_IDENTITY; // AVIF_MATRIX_COEFFICIENTS_BT2020_NCL;
+    image->matrixCoefficients = AVIF_MATRIX_COEFFICIENTS_BT2020_NCL;
     image->transformFlags = AVIF_TRANSFORM_IROT;
     image->irot.angle = 0;
-    image->yuvRange = AVIF_RANGE_FULL;
+    image->yuvRange = AVIF_RANGE_LIMITED;
     avifImageAllocatePlanes(image, AVIF_PLANES_YUV);
     {
       int plane_width = image->width;
@@ -38,8 +66,8 @@ void DumpPixelBuffer(const char* filename, uint16_t trc) {
         uint16_t* out_y_row = (uint16_t*)(image->yuvPlanes[AVIF_CHAN_Y] + y*image->yuvRowBytes[AVIF_CHAN_Y]);
         for (int x = 0;  x < plane_width; ++x) {
           uint16_t step = (x * kNumSteps) / plane_width;
-          uint16_t grey = 1024 * (step * 1.f / (kNumSteps - 1));
-          out_y_row[x] = grey;
+          float grey = step * 1.f / (kNumSteps - 1);
+          out_y_row[x] = 1024 * GreyToY(grey);
         }
       }
     }
@@ -51,9 +79,9 @@ void DumpPixelBuffer(const char* filename, uint16_t trc) {
         uint16_t* out_v_row = (uint16_t*)(image->yuvPlanes[AVIF_CHAN_V] + y*image->yuvRowBytes[AVIF_CHAN_V]);
         for (int x = 0; x < plane_width; ++x) {
           uint16_t step = (x * kNumSteps) / plane_width;
-          uint16_t grey = 1024 * (step * 1.f / (kNumSteps - 1));
-          out_u_row[x] = grey;
-          out_v_row[x] = grey;
+          float grey = step * 1.f / (kNumSteps - 1);
+          out_u_row[x] = 1024 * GreyToU(grey);
+          out_v_row[x] = 1024 * GreyToV(grey);
         }
       }
     }
