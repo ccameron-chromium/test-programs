30e48e66c978cd60876bbd2a2f7966008edfdebe
Christopher Cameron <ccameron@chromium.org>
2023-10-27 13:08:46 +0200

    Update staircase generator

--- staircase_generator.cc
+++ staircase_generator.cc
@@ -43,10 +43,54 @@ float GreyToV(float x) {
          BT2020_10bit_limited_rgb_to_yuv[10 + 4];
 }
 
-void DumpPixelBuffer(const char* filename, uint16_t trc) {
-  const int kNumSteps = 17;
-  int width = 64 * kNumSteps;
-  int height = 64;
+void RGBtoYUV(const float rgb[3], float yuv[3]) {
+  for (int i = 0; i < 3; ++i) {
+    yuv[i] = BT2020_10bit_limited_rgb_to_yuv[5*i + 4];
+    for (int j = 0; j < 3; ++j) {
+      yuv[i] += BT2020_10bit_limited_rgb_to_yuv[5*i + j] * rgb[j];
+    }
+  }
+}
+
+void GetRGB(uint16_t x_step, uint16_t num_x_steps, uint16_t y_step, float max_value, float rgb[3]) {
+  float grey = x_step * max_value / (num_x_steps - 1);
+  rgb[0] = rgb[1] = rgb[2] = 0.f;
+  switch (y_step) {
+    default:
+      rgb[0] = grey;
+      rgb[1] = grey;
+      rgb[2] = grey;
+      break;
+    case 1:
+      rgb[0] = grey;
+      break;
+    case 2:
+      rgb[1] = grey;
+      break;
+    case 3:
+      rgb[2] = grey;
+      break;
+    case 4:
+      rgb[0] = 0.5 * grey;
+      rgb[1] = 0.7 * grey;
+      rgb[2] = 0.9 * grey;
+      break;
+    case 5:
+      rgb[0] = grey;
+      rgb[1] = 1.0;
+      rgb[2] = grey;
+      break;
+    case 6:
+      rgb[0] = 1.f;
+      rgb[1] = grey;
+      rgb[2] = grey;
+      break;
+  }
+}
+
+void DumpPixelBuffer(const char* filename, uint16_t trc, int num_x_steps, int x_step_size, int num_y_steps, int y_step_size) {
+  int width = x_step_size * num_x_steps;
+  int height = y_step_size * num_y_steps;
 
     int returnCode = 1;
     avifRWData avifOutput = AVIF_DATA_EMPTY;
@@ -58,16 +102,29 @@ void DumpPixelBuffer(const char* filename, uint16_t trc) {
     image->transformFlags = AVIF_TRANSFORM_IROT;
     image->irot.angle = 0;
     image->yuvRange = AVIF_RANGE_LIMITED;
+
+    float max_value = 1.0;
+    if (trc == AVIF_TRANSFER_CHARACTERISTICS_SMPTE2084) {
+      max_value = 0.751827096247041;
+      image->clli.maxCLL = 1000;
+    }
+
     avifImageAllocatePlanes(image, AVIF_PLANES_YUV);
+
+
     {
       int plane_width = image->width;
       int plane_height = image->height;
       for (int y = 0; y < plane_height; ++y) {
         uint16_t* out_y_row = (uint16_t*)(image->yuvPlanes[AVIF_CHAN_Y] + y*image->yuvRowBytes[AVIF_CHAN_Y]);
         for (int x = 0;  x < plane_width; ++x) {
-          uint16_t step = (x * kNumSteps) / plane_width;
-          float grey = step * 1.f / (kNumSteps - 1);
-          out_y_row[x] = 1024 * GreyToY(grey);
+          uint16_t x_step = (x * num_x_steps) / plane_width;
+          uint16_t y_step = (y * num_y_steps) / plane_height;
+          float rgb[3];
+          GetRGB(x_step, num_x_steps, y_step, max_value, rgb);
+          float yuv[3];
+          RGBtoYUV(rgb, yuv);
+          out_y_row[x] = 1024 * yuv[0];
         }
       }
     }
@@ -78,10 +135,14 @@ void DumpPixelBuffer(const char* filename, uint16_t trc) {
         uint16_t* out_u_row = (uint16_t*)(image->yuvPlanes[AVIF_CHAN_U] + y*image->yuvRowBytes[AVIF_CHAN_U]);
         uint16_t* out_v_row = (uint16_t*)(image->yuvPlanes[AVIF_CHAN_V] + y*image->yuvRowBytes[AVIF_CHAN_V]);
         for (int x = 0; x < plane_width; ++x) {
-          uint16_t step = (x * kNumSteps) / plane_width;
-          float grey = step * 1.f / (kNumSteps - 1);
-          out_u_row[x] = 1024 * GreyToU(grey);
-          out_v_row[x] = 1024 * GreyToV(grey);
+          uint16_t x_step = (x * num_x_steps) / plane_width;
+          uint16_t y_step = (y * num_y_steps) / plane_height;
+          float rgb[3];
+          GetRGB(x_step, num_x_steps, y_step, max_value, rgb);
+          float yuv[3];
+          RGBtoYUV(rgb, yuv);
+          out_u_row[x] = 1024 * yuv[1];
+          out_v_row[x] = 1024 * yuv[2];
         }
       }
     }
@@ -137,8 +198,12 @@ void DumpPixelBuffer(const char* filename, uint16_t trc) {
 
 
 int main(int argc, char* argv[]) {
-  DumpPixelBuffer("staircase-srgb.avif", AVIF_TRANSFER_CHARACTERISTICS_SRGB);
-  DumpPixelBuffer("staircase-hlg.avif", AVIF_TRANSFER_CHARACTERISTICS_HLG);
-  DumpPixelBuffer("staircase-pq.avif", AVIF_TRANSFER_CHARACTERISTICS_SMPTE2084);
+  DumpPixelBuffer("staircase-rec2020-srgb.avif", AVIF_TRANSFER_CHARACTERISTICS_SRGB, 17, 64, 4, 64);
+  DumpPixelBuffer("staircase-rec2020-hlg.avif", AVIF_TRANSFER_CHARACTERISTICS_HLG, 17, 64, 4, 64);
+  DumpPixelBuffer("staircase-rec2020-pq.avif", AVIF_TRANSFER_CHARACTERISTICS_SMPTE2084, 17, 64, 4, 64);
+
+  DumpPixelBuffer("gradient-rec2020-srgb.avif", AVIF_TRANSFER_CHARACTERISTICS_SRGB, 1000, 1, 4, 64);
+  DumpPixelBuffer("gradient-rec2020-hlg.avif", AVIF_TRANSFER_CHARACTERISTICS_HLG, 1000, 1, 4, 64);
+  DumpPixelBuffer("gradient-rec2020-pq.avif", AVIF_TRANSFER_CHARACTERISTICS_SMPTE2084, 1000, 1, 4, 64);
   return 0;
 }
